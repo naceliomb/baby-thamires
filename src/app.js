f109ace3fdbc0fa0f6e1bf849fcd36015b37bcdf
Nacelio M. Barbosa <naceliombdev@gmail.com>
2023-12-01 20:04:21 -0300

    fix: added cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import voteRoutes from "./routes/voteRoutes.js";
 import { sequelize, synchronizeModels } from "./models/database.js";
+import cors from "cors";
 
 import { swaggerUi, swaggerDocs } from "../swagger.js";
 
@@ -8,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-
+app.use(cors());
 app.use("/api", voteRoutes);
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
