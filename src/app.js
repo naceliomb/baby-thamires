662238ae12e5ea65de516059ce809ccc60f2e022
thamiresmaia <thamiresmaia.sq@gmail.com>
2023-11-30 23:38:26 -0300

    melhorar

--- src/app.js
+++ src/app.js
@@ -8,7 +8,20 @@ const app = express();
 var cors = require('cors')
 const PORT = process.env.PORT || 3000;
 
-app.use(cors())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://192.168.1.103:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+  
+    // Intercepta as solicitações OPTIONS e retorna imediatamente para evitar a execução do resto do middleware
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+  
+    // Permite que o Express continue a execução do middleware pipeline
+    next();
+  });
 
 app.use(express.json());
 
